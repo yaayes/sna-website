ca3f61acb226b63383d4754be484ceb9468f1738
Yassine Sedrani <cxmoro@gmail.com>
2026-05-12 18:05:52 +0000

    feat: implement ShouldQueue interface for FormSubmission notifications

--- app/Notifications/FormSubmissionAdminNotification.php
+++ app/Notifications/FormSubmissionAdminNotification.php
@@ -5,10 +5,11 @@ namespace App\Notifications;
 use App\Models\FormSubmission;
 use App\Models\Payment;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class FormSubmissionAdminNotification extends Notification
+class FormSubmissionAdminNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- app/Notifications/FormSubmissionConfirmationNotification.php
+++ app/Notifications/FormSubmissionConfirmationNotification.php
@@ -5,10 +5,11 @@ namespace App\Notifications;
 use App\Models\FormSubmission;
 use App\Models\Payment;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class FormSubmissionConfirmationNotification extends Notification
+class FormSubmissionConfirmationNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
